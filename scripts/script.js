a4c7c7ab41ec671dd6faa8278571d746369d81a6
Jos3s <karatedkgt17@gmail.com>
2021-04-15 18:25:42 -0300

    Criação a operação de add ou remover transações

--- scripts/script.js
+++ scripts/script.js
@@ -24,6 +24,14 @@ const transactions=[
 
 const Transaction={
     all:transactions,
+    add(transaction){
+        Transaction.all.push(transaction);
+        App.reload();
+    },
+    remove(index){
+        Transaction.all.splice(index,1);
+        App.reload();
+    },
     incomes(){
         let income=0;
         Transaction.all.forEach(transaction => {
