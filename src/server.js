45f1b2816b95f15ce6be33e9983defd5c6f2c671
Jos3s <karatedkgt17@gmail.com>
2021-04-17 22:13:09 -0300

    Criando o routes e o server.js

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const path=require("path");
+
+const routes=require("./routes");
+
+const app=express();
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(routes);
+app.use(express.static("public"));
+
+app.listen(3000);
